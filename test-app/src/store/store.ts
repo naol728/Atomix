ec357d2f60f5380ccacbe6452b9b1648e015522f
naol <naolmeseret22@gmail.com>
2025-07-06 16:54:26 +0300

    fixing the error in test-app

---
+++ test-app/src/store/store.ts
@@ -0,0 +1,21 @@
+import { createStore } from "atomix-core";
+
+export interface InitalState {
+  count: number;
+}
+interface Actions {
+  increment: () => void;
+  decrement: () => void;
+  paramincrement: (param: number) => void;
+}
+const initalstate: InitalState = {
+  count: 0,
+};
+export const counterStore = createStore<InitalState, Actions>(
+  initalstate,
+  (set) => ({
+    increment: () => set((s) => ({ count: s.count + 1 })),
+    decrement: () => set((s) => ({ count: s.count - 1 })),
+    paramincrement: (param) => set((s) => ({ count: s.count + param })),
+  })
+);
